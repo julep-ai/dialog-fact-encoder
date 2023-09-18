917cfc9bbaa54a275642452b0ef480614fbc21f9
Diwank Singh Tomer <diwank.singh@gmail.com>
2023-09-18 17:29:41 +0530

    feat: Add parameters for query/passage encoding
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- embedder/__init__.py
+++ embedder/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from functools import lru_cache
+from typing import Any
 
 from fastapi import FastAPI
 from FlagEmbedding import FlagModel
@@ -11,8 +12,15 @@ from pydantic import BaseModel
 ## Model ##
 ###########
 
+model_names: list[str] = [
+    "BAAI/bge-small-en-v1.5",
+]
+
+default_model: str = model_names[0]
+
+
 @lru_cache(maxsize=1)
-def get_model(model_name: str = "BAAI/bge-small-en-v1.5"):
+def get_model(model_name: str):
     query_instruction_for_retrieval = (
         "Represent this sentence for searching relevant passages:"
     )
@@ -36,8 +44,9 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def lifespan():
-    # Load the ML model
-    get_model()
+    # Load the ML models
+    for name in model_names:
+        get_model(name)
 
 
 ###########
@@ -46,6 +55,10 @@ async def lifespan():
 
 class Request(BaseModel):
     instances: list[str]
+    parameters: dict[str, Any] = {
+        "model_name": default_model,
+        "query_type": "passage",
+    }
 
 
 ############
@@ -60,9 +73,19 @@ def health():
 @app.post("/embed")
 def embed(request: Request) -> dict[str, list[list[float]]]:
 
-    model = get_model()
-    predictions = model.encode(
-        request.instances,
+    # Constants
+    instances = request.instances
+    parameters = request.parameters
+    model_name = parameters.get("model_name", default_model)
+    query_type = parameters.get("query_type", "passage")
+
+    # Get the model
+    model = get_model(model_name)
+    encoder = model.encode_queries if query_type == "query" else model.encode
+
+    # Embed the instances
+    predictions = encoder(
+        instances,
         max_length=512,
     )
 
